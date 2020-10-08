f40d3e91ee0991e77eea9a0e7a99c496ef39edf3
lovit <soy.lovit@gmail.com>
2020-10-08 11:07:37 +0900

    Implement text load functions (#6)

--- moducorpus_sanitizer/utils.py
+++ moducorpus_sanitizer/utils.py
@@ -19,3 +19,46 @@ def check_fields(inputs, available_fields):
         else:
             checked.append(input)
     return checked
+
+
+def load_text(path):
+    with open(path, encoding='utf-8') as f:
+        docs = [doc.strip() for doc in f]
+    return docs
+
+
+def load_multiline_text(path):
+    with open(path, encoding='utf-8') as f:
+        docs = [doc.strip() for doc in f.read().split('\n\n')]
+    return docs
+
+
+def load_text_selectively(path, indices):
+    """Assume that `indices` is sorted"""
+    indices = set(indices)
+    docs = []
+    with open(path, encoding='utf-8') as f:
+        for idx, doc in enumerate(f):
+            if idx not in indices:
+                continue
+            docs.append(doc.strip())
+    return docs
+
+
+def load_multiline_text_selectively(path, indices):
+    """Assume that `indices` is sorted"""
+    indices = set(indices)
+    idx = 0
+    docs = []
+    buffer = []
+    with open(path, encoding='utf-8') as f:
+        for line in f:
+            if idx in indices:
+                buffer.append(line.strip())
+            if line == '\n':
+                idx += 1
+                if buffer:
+                    docs.append(buffer)
+                    buffer = []
+                continue
+    return docs
