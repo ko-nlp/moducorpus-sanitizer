1c165d2534e598feb8ab9e2a3bae5239268e1841
lovit <soy.lovit@gmail.com>
2021-02-13 03:01:27 +0900

    CLI 에서 형태 분석 말뭉치 지원 (#17)

--- moducorpus_sanitizer/cli.py
+++ moducorpus_sanitizer/cli.py
@@ -2,37 +2,47 @@ import argparse
 import os
 
 from .about import __version__
+from .modu_mp import mp_to_corpus
 from .modu_news import news_to_corpus
 
 
 def show_version(args):
-    print(f'moducorpus_sanitizer=={__version__}')
+    print(f"moducorpus_sanitizer=={__version__}")
 
 
 def show_arguments(args):
-    print('## Arguments of Moducorpus sanitizer ##')
+    print("## Arguments of Moducorpus sanitizer ##")
     for name, var in sorted(vars(args).items()):
         if callable(var):
-            print(f'  - {name} : {var.__name__}')
+            print(f"  - {name} : {var.__name__}")
         else:
-            print(f'  - {name} : {var}')
+            print(f"  - {name} : {var}")
 
 
 def main():
-    parser = argparse.ArgumentParser(description='moducorpus-sanitizer Command Line Interface')
+    parser = argparse.ArgumentParser(
+        description="moducorpus-sanitizer Command Line Interface"
+    )
     parser.set_defaults(func=show_version)
-    subparsers = parser.add_subparsers(help='moducorpus_sanitizer')
+    subparsers = parser.add_subparsers(help="moducorpus_sanitizer")
 
     # version
-    parser_version = subparsers.add_parser('version', help='Show version')
+    parser_version = subparsers.add_parser("version", help="Show version")
 
     # News
-    parser_news = subparsers.add_parser('news', help='News corpus')
-    parser_news.add_argument('--input_dir', required=True, type=str, help='path/to/NIKL_NEWSPAPER(v1.0)')
-    parser_news.add_argument('--output_dir', required=True, type=str, help='path/to/corpus/NIKL_NEWSPAPER(v1.0)')
-    parser_news.add_argument('--type', type=str, default='doublespaceline', choices=['multiline', 'doublespaceline'])
-    parser_news.add_argument('--fields', type=str, nargs='+', default=['title', 'paragraph'], choices=['title', 'paragraph'])
+    parser_news = subparsers.add_parser("news", help="News corpus")
+    parser_news.add_argument("-i", "--input_dir", required=True, type=str, help="path/to/NIKL_NEWSPAPER")
+    parser_news.add_argument("-o", "--output_dir", required=True, type=str, help="path/to/corpus/NIKL_NEWSPAPER")
+    parser_news.add_argument("-t", "--type", type=str, default="doublespaceline", choices=["multiline", "doublespaceline"])
+    parser_news.add_argument("--fields", type=str, nargs="+", default=["title", "paragraph"], choices=["title", "paragraph"])
     parser_news.set_defaults(func=news_to_corpus)
+
+    # Morpheme
+    parser_mp = subparsers.add_parser("mp", help="Morphological analysis corpus")
+    parser_mp.add_argument("-i", "--input_dir", required=True, type=str, help="path/to/NIKL_MP")
+    parser_mp.add_argument("-o", "--output_dir", required=True, type=str, help="path/to/corpus/")
+    parser_mp.add_argument("-t", "--type", default="eojeol", type=str, help="output type")
+    parser_mp.set_defaults(func=mp_to_corpus)
 
     # Do task
     args = parser.parse_args()
@@ -41,5 +51,5 @@ def main():
     task_function(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
